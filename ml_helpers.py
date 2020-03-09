89757c26889457ebf9badfaa00ffd985e2349b3e
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-09 11:16:37 +0100

    ...

--- ml_helpers.py
+++ ml_helpers.py
@@ -67,8 +67,10 @@ def load_bert_embeddings(X, max_length):
         last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple
         #print(last_hidden_states)
         print(last_hidden_states.shape)
-        print(last_hidden_states[0])
-        print(last_hidden_states[0][0])
+        #print(last_hidden_states[0])
+        #print(last_hidden_states[0][0])
+        last_hidden_states = last_hidden_states.reshape(last_hidden_states.shape[1], last_hidden_states.shape[2])
+        print(last_hidden_states.shape)
         X_bert_states_padded.append(last_hidden_states)
 
     print(len(X_bert_states_padded))
