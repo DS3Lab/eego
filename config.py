69079510f7dfff2cd1637f88497bfee7f7d87145
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-10 19:51:11 +0100

    :.

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class_task = 'sentiment-bin' #'sentiment-bin'
 feature_set = 'text_only' #'gaze_features'
 
 # word embeddings {none, glove (300d), bert}
-embeddings = 'bert'
+embeddings = 'glove'
 
 # hyper-parameters
 lstm_dim = [128]
