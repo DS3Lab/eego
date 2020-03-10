0817e9fee0eb241945c4df9e8f9a3ee5ec41785d
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-10 14:30:13 +0100

    :.

--- ml_helpers.py
+++ ml_helpers.py
@@ -116,6 +116,7 @@ def prepare_sequences_for_bert(X, max_seq_length):
 
     token_ids = map(lambda tids: tids + [0] * (max_seq_length - len(tids)), token_ids)
     token_ids = np.array(list(token_ids))
+    print(token_ids)
 
     return token_ids
 
