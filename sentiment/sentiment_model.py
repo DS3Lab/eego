f211b8b82aee51ffa6c1d96ab074a507918e2a54
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-12 19:38:26 +0100

    ..

--- sentiment/sentiment_model.py
+++ sentiment/sentiment_model.py
@@ -1,32 +1,54 @@
 import os
 import numpy as np
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Input, Dense, Dropout, GlobalAveragePooling1D, LSTM, Embedding
-from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.python.keras.utils import np_utils
 from tensorflow.python.keras.initializers import Constant
+import tensorflow.python.keras.backend as K
 import sklearn.metrics
 from sklearn.model_selection import KFold
 import ml_helpers
 import config
 import time
-from datetime import timedelta, date
-
-
-# Machine learning model for TERNARY sentiment classification
-
-seed_value = 42
-os.environ['KERAS_BACKEND'] = 'tensorflow'
-np.random.seed(seed_value)
+from datetime import timedelta
+import bert
 import tensorflow as tf
-tf.random.set_seed(seed_value)
-import os
-os.environ['PYTHONHASHSEED']=str(seed_value)
+
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+
+# Machine learning model for sentiment classification (binary and ternary)
+# Bert model
+
+def createBertLayer():
+    global bert_layer
+
+    # todo: model not the same as for tokenizer -- does it matter?
+    bertDir = os.path.join(config.modelBertDir, "multi_cased_L-12_H-768_A-12")
+
+    bert_params = bert.params_from_pretrained_ckpt(bertDir)
+
+    bert_layer = bert.BertModelLayer.from_params(bert_params, name="bert_layer")
+
+    bert_layer.apply_adapter_freeze()
+
+    print("Bert layer created")
 
 
-def lstm_classifier(features, labels, embedding_type, param_dict):
+"""
+def loadBertCheckpoint():
+    modelsFolder = os.path.join(modelBertDir, "multi_cased_L-12_H-768_A-12")
+    checkpointName = os.path.join(modelsFolder, "bert_model.ckpt")
+
+    bert.load_stock_weights(bert_layer, checkpointName)
+"""
+
+
+def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_value):
+
+    # set random seed
+    np.random.seed(random_seed_value)
+    tf.random.set_seed(random_seed_value)
+    os.environ['PYTHONHASHSEED'] = str(random_seed_value)
 
     start = time.time()
 
@@ -49,6 +71,7 @@ def lstm_classifier(features, labels, embedding_type, param_dict):
     tokenizer = Tokenizer(num_words=vocab_size)
     tokenizer.fit_on_texts(X)
     sequences = tokenizer.texts_to_sequences(X)
+    print(type(sequences))
     max_length = max([len(s) for s in sequences])
     print("max: ", max_length)
 
@@ -71,21 +94,19 @@ def lstm_classifier(features, labels, embedding_type, param_dict):
         print("Loading Glove embeddings...")
         embedding_dim = 300
         embedding_matrix = ml_helpers.load_glove_embeddings(vocab_size, word_index, embedding_dim)
-        print(embedding_matrix)
-        print(embedding_matrix.shape)
 
     if embedding_type is 'bert':
-        print("Loading Bert embeddings...")
-        bert_dim = 768
-        X_data = ml_helpers.load_bert_embeddings(X, max_length, bert_dim)
-        print("embeddings loaded")
+        print("Prepare sequences for Bert ...")
+        X_data_bert = ml_helpers.prepare_sequences_for_bert(X)
+        embedding_dim = 768
+
+        X_data = pad_sequences(X_data_bert, maxlen=max_length)
 
         print('Shape of data tensor:', X_data.shape)
         print('Shape of label tensor:', y.shape)
 
-
     # split data into train/test
-    kf = KFold(n_splits=config.folds, random_state=seed_value, shuffle=True)
+    kf = KFold(n_splits=config.folds, random_state=random_seed_value, shuffle=True)
 
     fold = 0
     fold_results = {}
@@ -93,92 +114,95 @@ def lstm_classifier(features, labels, embedding_type, param_dict):
     for train_index, test_index in kf.split(X_data):
 
         print("FOLD: ", fold)
-
-        #print(np.array(X_data))
-
-        #print(np.array(X_data)[train_index])
-
         # print("TRAIN:", train_index, "TEST:", test_index)
-        # todo: why does this take so much time for BErt??
-        print("splitting X")
+        print("splitting train and test data...")
         y_train, y_test = y[train_index], y[test_index]
         X_train, X_test = X_data[train_index], X_data[test_index]
-        print("splitting y")
-
-
-        print(X_train.shape)
-        #print(X_train[0].shape)
-        print(X_test.shape)  # test samples
-        print(y_train.shape)
-        print(y_test.shape)  # test labels
-
-        print(X_train[0])
-        print(type(X_train[0]))
 
         # reset model
         K.clear_session()
 
         lstm_dim = param_dict['lstm_dim']
+        lstm_layers = param_dict['lstm_layers']
         dense_dim = param_dict['dense_dim']
         dropout = param_dict['dropout']
         batch_size = param_dict['batch_size']
         epochs = param_dict['epochs']
         lr = param_dict['lr']
 
-        fold_results['params'] = [lstm_dim, dense_dim, dropout, batch_size, epochs, lr, embedding_type]
+        fold_results['params'] = [lstm_dim, lstm_layers, dense_dim, dropout, batch_size, epochs, lr, embedding_type, random_seed_value]
 
+        # define model
         print("Preparing model...")
-
-        model = Sequential()
+        #model = tf.keras.Sequential()
 
         if embedding_type is 'none':
-            embedding_layer = Embedding(num_words, 32, input_length=max_length, name='none_input_embeddings')
+            # todo: tune embedding dim?
+            embedding_layer = tf.keras.layers.Embedding(num_words, 32, input_length=max_length, name='none_input_embeddings')
+            model.add(embedding_layer)
 
         elif embedding_type is 'glove':
             # load pre-trained word embeddings into an Embedding layer
             # note that we set trainable = False so as to keep the embeddings fixed
-            embedding_layer = Embedding(num_words,
+            embedding_layer = tf.keras.layers.Embedding(num_words,
                                         embedding_dim,
                                         embeddings_initializer=Constant(embedding_matrix),
                                         input_length=max_length,
                                         trainable=False,
                                         name='glove_input_embeddings')
+            model.add(embedding_layer)
+
         elif embedding_type is 'bert':
-            #embedding_layer = Embedding(num_words, bert_dim, input_length=max_length, trainable=False, name='bert_input_embeddings')
-            # todo: try this
-            # https://medium.com/analytics-vidhya/bert-in-keras-tensorflow-2-0-using-tfhub-huggingface-81c08c5f81d8
-            #embedding_input = Input(shape=(max_length, bert_dim), name='bert_input_embeddings')
 
-            model = tf.keras.Sequential([
-                tf.keras.layers.Input(shape=(max_length,), dtype='int32', name='input_ids'),
-                ml_helpers.bert_layer,
-                tf.keras.layers.Flatten(),
-                tf.keras.layers.Dense(256, activation=tf.nn.relu),
-                tf.keras.layers.Dropout(0.5),
-                tf.keras.layers.Dense(256, activation=tf.nn.relu),
-                tf.keras.layers.Dropout(0.5),
-                tf.keras.layers.Dense(y_train.shape[1], activation=tf.nn.softmax)
-            ])
+            createBertLayer()
 
-            model.build(input_shape=(None, max_length))
+            def createModel():
+                global model
 
-        #model.add(embedding_layer)
+                model = tf.keras.Sequential([
+                    tf.keras.layers.Input(shape=(max_length,), dtype='int32', name='input_ids'),
+                    bert_layer,
+                    tf.keras.layers.Flatten(),
+                    #tf.keras.layers.Dense(256, activation=tf.nn.relu),
+                    #tf.keras.layers.Dropout(0.5),
+                    #tf.keras.layers.Dense(256, activation=tf.nn.relu),
+                    #tf.keras.layers.Dropout(0.5),
+                    #tf.keras.layers.Dense(y_train.shape[1], activation=tf.nn.softmax)
+                    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(lstm_dim)),
+                    tf.keras.layers.Dense(dense_dim, activation='relu'),
+                    tf.keras.layers.Dropout(rate=dropout),
+                    tf.keras.layers.Dense(y_train.shape[1], activation='softmax'),
+                ])
+
+                model.build(input_shape=(None, max_length))
+
+                model.compile(loss='categorical_crossentropy',
+                              optimizer=tf.keras.optimizers.Adam(lr=lr),
+                              metrics=['accuracy'])
+
+                print(model.summary())
+
+            createModel()
+
         #model.summary()
-        #model.add(LSTM(lstm_dim))
-        #model.add(Dense(dense_dim, activation='relu'))
-        #model.add(Dropout(rate=dropout))
-        #model.add(Dense(y_train.shape[1], activation='softmax'))
 
-        model.compile(loss='categorical_crossentropy',
-                      optimizer=tf.keras.optimizers.Adam(lr=lr),
-                      metrics=['accuracy'])
+        #for l in list(range(lstm_layers)):
+         #   if l < lstm_layers-1:
+          #      model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(lstm_dim, return_sequences=True)))
+           # else:
+            #    model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(lstm_dim)))
+        #model.add(tf.keras.layers.Dense(dense_dim, activation='relu'))
+        #model.add(tf.keras.layers.Dropout(rate=dropout))
+        #model.add(tf.keras.layers.Dense(y_train.shape[1], activation='softmax'))
 
-        model.summary()
+        #model.compile(loss='categorical_crossentropy',
+         #             optimizer=tf.keras.optimizers.Adam(lr=lr),
+          #            metrics=['accuracy'])
+
+        #model.summary()
 
         # train model
         history = model.fit(X_train, y_train, validation_split=0.1, epochs=epochs, batch_size=batch_size)
-        print(history)
-        # todo: add final validation accuracy + loss to fold results
 
         # evaluate model
         scores = model.evaluate(X_test, y_test, verbose=0)
@@ -187,18 +211,26 @@ def lstm_classifier(features, labels, embedding_type, param_dict):
         rounded_predictions = [np.argmax(p) for p in predictions]
         rounded_labels = np.argmax(y_test, axis=1)
         p, r, f, support = sklearn.metrics.precision_recall_fscore_support(rounded_labels, rounded_predictions, average='macro')
-        print(p, r, f)
-        conf_matrix = sklearn.metrics.confusion_matrix(rounded_labels, rounded_predictions)
-        print(conf_matrix)
+        #print(p, r, f)
+        #conf_matrix = sklearn.metrics.confusion_matrix(rounded_labels, rounded_predictions)
+        #print(conf_matrix)
 
         if fold == 0:
-            fold_results['loss'] = [scores[0]]
+            fold_results['train-loss'] = [history.history['loss']]
+            fold_results['train-accuracy'] = [history.history['accuracy']]
+            fold_results['val-loss'] = [history.history['val_loss']]
+            fold_results['val-accuracy'] = [history.history['val_accuracy']]
+            fold_results['test-loss'] = [scores[0]]
             fold_results['test-accuracy'] = [scores[1]]
             fold_results['precision'] = [p]
             fold_results['recall'] = [r]
             fold_results['fscore'] = [f]
         else:
-            fold_results['loss'].append(scores[0])
+            fold_results['train-loss'].append(history.history['loss'])
+            fold_results['train-accuracy'].append(history.history['accuracy'])
+            fold_results['val-loss'].append(history.history['val_loss'])
+            fold_results['val-accuracy'].append(history.history['val_accuracy'])
+            fold_results['test-loss'].append(scores[0])
             fold_results['test-accuracy'].append(scores[1])
             fold_results['precision'].append(p)
             fold_results['recall'].append(r)
