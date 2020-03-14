b297d5c16cafa91ecfcf0e16010515528845d9c2
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-14 18:47:21 +0100

    ..

--- feature_extraction/zuco_reader.py
+++ feature_extraction/zuco_reader.py
@@ -63,6 +63,7 @@ def extract_labels(feature_dict, label_dict, task, subject):
             for line in ner_ground_truth:
                 sent_tokens = []
                 sent_labels = []
+                print(line)
 
                 # start of new sentence
                 if line == '\n':
@@ -84,6 +85,7 @@ def extract_labels(feature_dict, label_dict, task, subject):
                     line = line.split('\t')
                     sent_tokens.append(line[0])
                     sent_labels.append(line[1])
+                    print(sent_tokens)
 
             print('ZuCo 1 sentences not found:', count)
 
