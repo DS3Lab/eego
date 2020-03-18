962b8d6594b97f2a071e64aad6dee94234ba03d8
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-18 22:11:00 +0100

    ..

--- reldetect/reldetect_model.py
+++ reldetect/reldetect_model.py
@@ -33,49 +33,7 @@ def createBertLayer():
     print("Bert layer created")
 
 
-def macro_f1(y, y_hat, thresh=0.5):
-    """Compute the macro F1-score on a batch of observations (average F1 across labels)
 
-    Args:
-        y (int32 Tensor): labels array of shape (BATCH_SIZE, N_LABELS)
-        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
-        thresh: probability value above which we predict positive
-
-    Returns:
-        macro_f1 (scalar Tensor): value of macro F1 for the batch
-    """
-    y_pred = tf.cast(tf.greater(y_hat, thresh), tf.float32)
-    tp = tf.cast(tf.math.count_nonzero(y_pred * y, axis=0), tf.float32)
-    fp = tf.cast(tf.math.count_nonzero(y_pred * (1 - y), axis=0), tf.float32)
-    fn = tf.cast(tf.math.count_nonzero((1 - y_pred) * y, axis=0), tf.float32)
-    f1 = 2 * tp / (2 * tp + fn + fp + 1e-16)
-    macro_f1 = tf.reduce_mean(f1)
-    return macro_f1
-
-
-def macro_soft_f1(y, y_hat):
-    """Compute the macro soft F1-score as a cost.
-    Average (1 - soft-F1) across all labels.
-    Use probability values instead of binary predictions.
-
-    Args:
-        y (int32 Tensor): targets array of shape (BATCH_SIZE, N_LABELS)
-        y_hat (float32 Tensor): probability matrix of shape (BATCH_SIZE, N_LABELS)
-
-    Returns:
-        cost (scalar Tensor): value of the cost function for the batch
-    """
-
-    y = tf.cast(y, tf.float32)
-    y_hat = tf.cast(y_hat, tf.float32)
-    tp = tf.reduce_sum(y_hat * y, axis=0)
-    fp = tf.reduce_sum(y_hat * (1 - y), axis=0)
-    fn = tf.reduce_sum((1 - y_hat) * y, axis=0)
-    soft_f1 = 2 * tp / (2 * tp + fn + fp + 1e-16)
-    cost = 1 - soft_f1  # reduce 1 - soft-f1 in order to increase soft-f1
-    macro_cost = tf.reduce_mean(cost)  # average on all labels
-
-    return macro_cost
 
 def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_value):
 
@@ -232,15 +190,11 @@ def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_va
                 model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(lstm_dim)))
         model.add(tf.keras.layers.Dense(dense_dim, activation='relu'))
         model.add(tf.keras.layers.Dropout(rate=dropout))
-        model.add(tf.keras.layers.Dense(y_train.shape[1], activation='sigmoid'))
+        model.add(tf.keras.layers.Dense(y_train.shape[1], activation='softmax'))
 
-        #model.compile(loss='categorical_crossentropy',
-         #             optimizer=tf.keras.optimizers.Adam(lr=lr),
-          #            metrics=['accuracy'])
-
-        model.compile(loss=macro_soft_f1,
+        model.compile(loss='categorical_crossentropy',
                       optimizer=tf.keras.optimizers.Adam(lr=lr),
-                      metrics=[macro_f1])
+                      metrics=['accuracy'])
 
         model.summary()
 
