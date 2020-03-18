585035e50c83f84a3b566f6b0dbfb73292c22913
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-18 22:49:19 +0100

    ..

--- reldetect/reldetect_model.py
+++ reldetect/reldetect_model.py
@@ -192,11 +192,15 @@ def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_va
                 model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(lstm_dim)))
         model.add(tf.keras.layers.Dense(dense_dim, activation='relu'))
         model.add(tf.keras.layers.Dropout(rate=dropout))
-        model.add(tf.keras.layers.Dense(y_train.shape[1], activation='softmax'))
+        model.add(tf.keras.layers.Dense(y_train.shape[1], activation='sigmoid'))
 
-        model.compile(loss='categorical_crossentropy',
+        model.compile(loss='binary_crossentropy',
                       optimizer=tf.keras.optimizers.Adam(lr=lr),
                       metrics=['accuracy'])
+
+        # todo: try
+        #model.compile(loss="binary_crossentropy", optimizer=opt,
+	    #metrics=["accuracy"])
 
         model.summary()
 
