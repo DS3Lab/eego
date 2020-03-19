24f84fb8f6a02d0b357c4c0e74ce67cfcf1f7c3d
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-19 09:09:52 +0100

    ..

--- reldetect/reldetect_model.py
+++ reldetect/reldetect_model.py
@@ -40,16 +40,15 @@ def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_va
     vocab_size = 100000
 
     # prepare text samples
-    print('Processing text dataset')
+    print('Processing text dataset...')
 
-    print('Found %s texts.' % len(X))
+    print('Found %s sentences.' % len(X))
 
     tokenizer = Tokenizer(num_words=vocab_size)
     tokenizer.fit_on_texts(X)
     sequences = tokenizer.texts_to_sequences(X)
-    print(type(sequences))
     max_length = max([len(s) for s in sequences])
-    print("max: ", max_length)
+    print("Max sentence length: ", max_length)
 
     word_index = tokenizer.word_index
     print('Found %s unique tokens.' % len(word_index))
@@ -180,7 +179,6 @@ def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_va
         model.compile(loss='binary_crossentropy',
                       optimizer=tf.keras.optimizers.Adam(lr=lr),
                       metrics=[tf.keras.metrics.Precision(), 'accuracy'])
-                      #metrics=['accuracy'])
 
         model.summary()
 
@@ -208,6 +206,10 @@ def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_va
         print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
         print("-----")
 
+        # check confusion matrix
+        conf_matrix = sklearn.metrics.confusion_matrix(y_test, pred)
+        print(conf_matrix)
+
         # save results
         if fold == 0:
             fold_results['train-loss'] = [history.history['loss']]
@@ -219,7 +221,7 @@ def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_va
             fold_results['precision'] = [precision]
             fold_results['recall'] = [recall]
             fold_results['fscore'] = [f1]
-            fold_results['threshold'] = [threshold]
+            fold_results['threshold'] = threshold
         else:
             fold_results['train-loss'].append(history.history['loss'])
             fold_results['train-accuracy'].append(history.history['accuracy'])
@@ -230,13 +232,12 @@ def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_va
             fold_results['precision'].append(precision)
             fold_results['recall'].append(recall)
             fold_results['fscore'].append(f1)
-            fold_results['threshold'] = [threshold]
 
         fold += 1
 
     elapsed = (time.time() - start)
     print("Training time (all folds):", str(timedelta(seconds=elapsed)))
-    fold_results['training_time'] = [elapsed]
+    fold_results['training_time'] = elapsed
 
     return fold_results
 
