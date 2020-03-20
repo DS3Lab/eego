6f426176d3cca6c6ccbcc2b2825b3551cacd4366
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-20 10:53:34 +0100

    adding eeg features

--- feature_extraction/eeg_extractor.py
+++ feature_extraction/eeg_extractor.py
@@ -5,8 +5,8 @@ import nltk
 import numpy as np
 
 
-# get raw EEG features (mean word level activity)
-def extract_raw_eeg(sentence_data, sentence_dict):
+# get raw EEG features (mean sentence level activity)
+def extract_raw_eeg(sentence_data, eeg_dict):
     """extract tokens of all sentences."""
 
     for tup in sentence_data:
@@ -21,32 +21,19 @@ def extract_raw_eeg(sentence_data, sentence_dict):
 
             raw_sent_eeg_ref = rawData[idx][0]
             raw_sent_eeg = f[raw_sent_eeg_ref]
-            #print(type(raw_sent_eeg))
-            print(raw_sent_eeg.shape)
             mean_raw_sent_eeg = np.nanmean(raw_sent_eeg, axis=0)
-            print(mean_raw_sent_eeg.shape)
 
             obj_reference_content = contentData[idx][0]
             sent = dh.load_matlab_string(f[obj_reference_content])
-            # whitespace tokenization
-            split_tokens = sent.split()
-            # linguistic tokenization
-            spacy_tokens = nltk.word_tokenize(sent)
 
             # for sentiment and relation detection
             if config.class_task.startswith('sentiment') or config.class_task == "reldetect":
-                if sent not in sentence_dict:
-                    sentence_dict[sent] = split_tokens
+                if sent not in eeg_dict:
+                    eeg_dict[sent] = {'mean_raw_sent_eeg': mean_raw_sent_eeg}
                 else:
                     print('duplicate!')
 
             # for ner (different tokenization needed for NER)
-            if config.class_task == "ner":
-                if sent not in sentence_dict:
-                    sentence_dict[sent] = spacy_tokens
+            #if config.class_task == "ner":
+            # todo: how to handle for word-level models?
 
-                else:
-                    print('duplicate!')
-
-
-# get raw EEG features for each power spectrum
