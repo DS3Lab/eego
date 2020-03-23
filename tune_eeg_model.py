aa7ba32d5b106d5ed69b855a9234527dd4b437b6
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-23 20:39:03 +0100

    eeg only model

--- tune_eeg_model.py
+++ tune_eeg_model.py
@@ -55,8 +55,17 @@ def main():
                                             save_results(fold_results, config.class_task)
 
                                         elif config.class_task == 'sentiment-tri':
-                                            fold_results = sentiment_model.lstm_classifier(feature_dict, label_dict,
-                                                                                           emb, parameter_dict, rand)
+                                            if 'combi_concat' in config.feature_set:
+                                                print("Starting EEG + text combi model")
+                                                fold_results = sentiment_combi_model.lstm_classifier(feature_dict,
+                                                                                                   label_dict, eeg_dict,
+                                                                                                   emb, parameter_dict,
+                                                                                                   rand)
+                                            else:
+                                                fold_results = sentiment_eeg_model.lstm_classifier(feature_dict,
+                                                                                                   label_dict, eeg_dict,
+                                                                                                   emb, parameter_dict,
+                                                                                                   rand)
                                             save_results(fold_results, config.class_task)
                                         elif config.class_task == 'sentiment-bin':
                                             for s, label in list(label_dict.items()):
