d75b2a572ae802c0793b28c058e2668821ccf55a
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-25 14:21:29 +0100

    gaze model

--- feature_extraction/gaze_extractor.py
+++ feature_extraction/gaze_extractor.py
@@ -49,14 +49,17 @@ def word_level_et_features(sentence_data, gaze_dict):
                 print("NO sentence data available!")
 
             # for sentiment and relation detection
-            if sent_features:
-                print(sent_features)
-                if config.class_task.startswith('sentiment') or config.class_task == "reldetect":
-                    if sent not in gaze_dict:
-                        gaze_dict[sent] = {}
-                        for widx, fts in sent_features.items():
+            #if sent_features:
+             #   print(sent_features)
+            if config.class_task.startswith('sentiment') or config.class_task == "reldetect":
+                if sent not in gaze_dict:
+                    gaze_dict[sent] = {}
+                    for widx, fts in sent_features.items():
+                        gaze_dict[sent][widx] = [fts]
+                else:
+                    for widx, fts in sent_features.items():
+                        if not widx in gaze_dict[sent]:
                             gaze_dict[sent][widx] = [fts]
-                    else:
-                        for widx, fts in gaze_dict[sent].items():
+                        else:
                             gaze_dict[sent][widx].append(sent_features[widx])
 
