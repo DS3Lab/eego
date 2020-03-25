4209e81219d434919f0d8f04a76720df6b77127d
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-25 17:09:50 +0100

    ner fix tokenization

--- ner/ner_text_model.py
+++ ner/ner_text_model.py
@@ -30,12 +30,7 @@ def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_va
 
     start = time.time()
 
-    X = list(features.keys())
     y = list(labels.values())
-    X_tokenized = list(features.values())
-    print(X[0])
-    print(len(X_tokenized[0]))
-    print(len(X_tokenized[1]))
 
     vocab_size = 4633
     sequences = []
@@ -48,33 +43,18 @@ def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_va
                 word_index[token] = number
         sequences.append(seq)
 
-    print(len(sequences[0]))
-    print(len(sequences[1]))
-
-    print(len(y[0]))
-    print(len(y[1]))
-
     label_names = {0: 'O', 1: 'B-PER', 2: 'I-PER', 3: 'B-ORG', 4: 'I-ORG', 5: 'B-LOC', 6: 'I-LOC'}
 
     # plot sample distribution
     # ml_helpers.plot_label_distribution(y)
-
-    #vocab_size = 100000
 
     # prepare text samples
     print('Processing text dataset...')
 
     print('Found %s sentences.' % len(sequences))
 
-    #tokenizer = Tokenizer(num_words=vocab_size)
-    #tokenizer.fit_on_texts(X)
-    #print(X[0])
-    #sequences = tokenizer.texts_to_sequences(X)
-    #print(sequences[0])
     max_length = max([len(s) for s in sequences])
 
-    #word_index = tokenizer.word_index
-    #print(word_index)
     print('Found %s unique tokens.' % len(word_index))
     num_words = min(vocab_size, len(word_index) + 1)
 
