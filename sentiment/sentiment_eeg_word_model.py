87a62ecde64ce01864ef940fa4b4f67f6d3d6795
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-26 09:14:37 +0100

    eeg model

--- sentiment/sentiment_eeg_word_model.py
+++ sentiment/sentiment_eeg_word_model.py
@@ -12,7 +12,11 @@ import config
 import time
 from datetime import timedelta
 import tensorflow as tf
-import bert
+from tensorflow.python.keras.layers import Input, Dense, concatenate, Embedding, LSTM, Bidirectional, Flatten, Dropout
+from tensorflow.python.keras.models import Model
+import json
+
+
 
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 
@@ -20,14 +24,12 @@ os.environ['KERAS_BACKEND'] = 'tensorflow'
 # Machine learning model for sentiment classification (binary and ternary)
 # Learning on EEG data only!
 
-def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_seed_value):
+def lstm_classifier(labels, eeg, embedding_type, param_dict, random_seed_value):
 
     # set random seed
     np.random.seed(random_seed_value)
     tf.random.set_seed(random_seed_value)
     os.environ['PYTHONHASHSEED'] = str(random_seed_value)
-
-    start = time.time()
 
     y = list(labels.values())
 
@@ -37,22 +39,42 @@ def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_se
     # convert class labels to one hot vectors
     y = np_utils.to_categorical(y)
 
-    # prepare EEG data
-    # prepare EEG data
-    eeg_X = []
-    #print(eeg)
+    start = time.time()
+
+    # prepare eye-tracking data
+    gaze_X = []
+    max_len = 0
+
+    # save eeg feats
+    eeg_feats_file = open('eeg_raw_word_feats_file.json', 'w')
+    json.dump(eeg, eeg_feats_file)
+
     for s in eeg.values():
         # average over all subjects
         sent_feats = []
+        max_len = len(s) if len(s) > max_len else max_len
         for w, fts in s.items():
-            print(len(fts))
-            print(fts)
             subj_mean_word_feats = np.nanmean(fts, axis=0)
-            print(w, subj_mean_word_feats)
+            subj_mean_word_feats[np.isnan(subj_mean_word_feats)] = 0.0
             sent_feats.append(subj_mean_word_feats)
-        print(len(sent_feats))
-        eeg_X.append(sent_feats)
-    print(len(eeg_X))
+        gaze_X.append(sent_feats)
+
+    # todo scale features?
+
+    # pad gaze sequences
+    for s in gaze_X:
+        while len(s) < max_len:
+            s.append(np.zeros(5))
+
+    X_data_gaze = np.array(gaze_X)
+    print(X_data_gaze.shape)
+
+    max_length_gaze = max_len
+
+
+
+    X_data = X_data_gaze
+    max_length = max_length_gaze
 
     # split data into train/test
     kf = KFold(n_splits=config.folds, random_state=random_seed_value, shuffle=True)
@@ -89,14 +111,17 @@ def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_se
 
         # define model
         print("Preparing model...")
-        model = tf.keras.Sequential()
+        input_text = Input(shape=(X_train.shape[1], X_train.shape[2]), dtype=tf.float64, name='gaze_input_tensor')
+        # todo: change type of all layers to tf.float64?
+        text_model = Bidirectional(LSTM(lstm_dim, return_sequences=True))(input_text)
+        for _ in list(range(lstm_layers-1)):
+            text_model = Bidirectional(LSTM(lstm_dim, recurrent_dropout=0.2, dropout=0.2, return_sequences=True))(text_model)
+        text_model = Flatten()(text_model)
+        text_model = Dense(dense_dim, activation="relu")(text_model)
+        text_model = Dropout(dropout)(text_model)
+        text_model = Dense(y_train.shape[1], activation="softmax")(text_model)
 
-        model.add(tf.keras.layers.Input(shape=(max_length,), dtype='int32', name='input_eeg'))
-        model.add(tf.keras.layers.Dense(dense_dim, activation=tf.nn.relu))
-        model.add(tf.keras.layers.Dropout(dropout))
-        model.add(tf.keras.layers.Dense(dense_dim, activation=tf.nn.relu))
-        model.add(tf.keras.layers.Dropout(dropout))
-        model.add(tf.keras.layers.Dense(y_train.shape[1], activation=tf.nn.softmax))
+        model = Model(inputs=input_text, outputs=text_model)
 
         model.compile(loss='categorical_crossentropy',
                       optimizer=tf.keras.optimizers.Adam(lr=lr),
