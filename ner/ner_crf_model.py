2a93c34ca432b836abc4eb4ee22c0a4d0046f3f6
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-26 17:55:50 +0100

    ner debugging

--- ner/ner_crf_model.py
+++ ner/ner_crf_model.py
@@ -181,7 +181,7 @@ def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_va
         text_model = TimeDistributed(Dense(len(label_names), activation='softmax'))(text_model)
         print(text_model.shape)
 
-        crf = tfa.text.crf.CrfDecodeForwardRnnCell(text_model)  # CRF layer
+        crf = tfa.text.crf.CrfDecodeForwardRnnCell()(text_model)  # CRF layer
         out = crf(text_model)
 
         model = Model(inputs=input_list, outputs=out)
