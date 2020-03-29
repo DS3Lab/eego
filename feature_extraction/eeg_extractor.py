56d2d8efd1be00009c101ef44c9354787c889bb3
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-29 20:50:14 +0200

    ..

--- feature_extraction/eeg_extractor.py
+++ feature_extraction/eeg_extractor.py
@@ -98,6 +98,9 @@ def extract_word_band_eeg(sentence_data, eeg_dict):
                         word_t1 = word_data[widx]["TRT_t1"]
                         word_t2 = word_data[widx]["TRT_t2"]
                         word_t = (word_t1 + word_t2) / 2
+                        print(word_t.shape)
+                        word_t = word_t.transpose()
+                        print(word_t.shape)
                         sent_features[widx] = word_t
 
                     else:
