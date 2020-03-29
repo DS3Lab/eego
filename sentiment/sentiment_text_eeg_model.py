0899c6a34ae91b87eed068d08ecff39985f1da7e
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-03-29 21:43:09 +0200

    ..

--- sentiment/sentiment_text_eeg_model.py
+++ sentiment/sentiment_text_eeg_model.py
@@ -199,7 +199,7 @@ def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_se
         combined = dot([text_model_model.output, cognitive_model_model.output], axes=1, normalize=True)
         # apply another dense layer and then a softmax prediction on the combined outputs
         # todo: does this layer help?
-        #combined = Dense(8, activation="relu", name="final_dense")(combined)
+        combined = Dense(8, activation="relu", name="final_dense")(combined)
         combi_model = Dense(y_train.shape[1], activation="softmax")(combined)
 
         model = Model(inputs=[text_model_model.input, cognitive_model_model.input], outputs=combi_model)
