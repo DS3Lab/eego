06ae36610b6970513c12e485c2aafdf52ed7254e
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-04-21 20:14:32 +0200

    ..

--- feature_extraction/eeg_extractor.py
+++ feature_extraction/eeg_extractor.py
@@ -40,12 +40,14 @@ def extract_word_raw_eeg(sentence_data, eeg_dict):
                                 word_eeg.append(fix)
                         word_eeg = np.nanmean(word_eeg, axis=0)
                         if word_eeg.shape == (105,):
+                            word_eeg = [float(n) for n in word_eeg]
                             sent_features[widx] = list(word_eeg)
 
                     if widx not in sent_features:
                         nan_array = np.empty((105,))
                         nan_array[:] = np.NaN
-                        sent_features[widx] = list(nan_array)
+                        nan_array = [float(n) for n in nan_array]
+                        sent_features[widx] = nan_array
 
             except ValueError:
                 print("NO sentence data available!")
