53723f58410d90cc080e9a950f3c3ceb2c35fee1
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-04-23 14:55:44 +0200

    ..

--- sentiment/sentiment_text_model.py
+++ sentiment/sentiment_text_model.py
@@ -211,6 +211,6 @@ def lstm_classifier(features, labels, embedding_type, param_dict, random_seed_va
     conf_matrix = sklearn.metrics.confusion_matrix(all_labels, all_predictions)  # todo: add labels
     print(conf_matrix)
     ml_helpers.plot_confusion_matrix(conf_matrix)
-    ml_helpers.plot_prediction_distribution(all_predictions)
+    ml_helpers.plot_prediction_distribution(all_labels, all_predictions)
 
     return fold_results
