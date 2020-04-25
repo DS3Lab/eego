20804b5c55c5fb78c1b79a4e25032568b704fa0e
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-04-25 15:38:00 +0200

    ..

--- reldetect/reldetect_text_eeg_model.py
+++ reldetect/reldetect_text_eeg_model.py
@@ -6,7 +6,8 @@ from tensorflow.python.keras.initializers import Constant
 import tensorflow.python.keras.backend as K
 from tensorflow.python.keras.layers import Input, Dense, Embedding, LSTM, Bidirectional, Flatten, Dropout
 from tensorflow.python.keras.layers.merge import concatenate
-from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.models import Model, load_model
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
 import sklearn.metrics
 from sklearn.model_selection import KFold
 import ml_helpers
@@ -14,6 +15,9 @@ import config
 import time
 from datetime import timedelta
 import tensorflow as tf
+import datetime
+
+d = datetime.datetime.today()
 
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 
@@ -117,6 +121,8 @@ def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_se
 
     fold = 0
     fold_results = {}
+    all_labels = []
+    all_predictions = []
 
     for train_index, test_index in kf.split(X_data_text):
 
@@ -209,14 +215,24 @@ def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_se
 
         model.summary()
 
+        # callbacks for early stopping and saving the best model
+        es = EarlyStopping(monitor='val_accuracy', mode='max', min_delta=config.min_delta, patience=config.patience)
+        model_name = '../models/' + str(random_seed_value) + '_fold' + str(fold) + '_' + config.class_task + '_' + \
+                     config.feature_set[0] + '_' + config.embeddings[0] + '_' + d.strftime(
+            '%d-%m-%Y') + '.h5'
+        mc = ModelCheckpoint(model_name, monitor='val_accuracy', mode='max', save_best_only=True, verbose=1)
+
         # train model
         history = model.fit([X_train_text, X_train_eeg] if embedding_type is not 'bert' else [X_train_text, X_train_masks, X_train_eeg], y_train,
-                            validation_split=0.1, epochs=epochs, batch_size=batch_size)
+                            validation_split=0.1, epochs=epochs, batch_size=batch_size, callbacks=[es, mc])
+        print("Best epoch:", len(history.history['loss']) - config.patience)
 
         # evaluate model
-        scores = model.evaluate([X_test_text, X_test_eeg] if embedding_type is not 'bert' else [X_test_text, X_test_masks, X_test_eeg], y_test,
+        # load the best saved model
+        saved_model = load_model(model_name)
+        scores = saved_model.evaluate([X_test_text, X_test_eeg] if embedding_type is not 'bert' else [X_test_text, X_test_masks, X_test_eeg], y_test,
                                 verbose=0)
-        predictions = model.predict([X_test_text, X_test_eeg] if embedding_type is not 'bert' else [X_test_text, X_test_masks, X_test_eeg])
+        predictions = saved_model.predict([X_test_text, X_test_eeg] if embedding_type is not 'bert' else [X_test_text, X_test_masks, X_test_eeg])
 
         print("For threshold:", threshold)
         pred = predictions.copy()
@@ -233,6 +249,9 @@ def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_se
         per_class_results = sklearn.metrics.classification_report(y_test, pred, target_names=label_names,
                                                                   output_dict=True)
 
+        all_labels += list(y_test)
+        all_predictions += list(pred)
+
         if fold == 0:
             fold_results['train-loss'] = [history.history['loss']]
             fold_results['train-accuracy'] = [history.history['accuracy']]
@@ -244,6 +263,8 @@ def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_se
             fold_results['recall'] = [r]
             fold_results['fscore'] = [f]
             fold_results['threshold'] = threshold
+            fold_results['model'] = [model_name]
+            fold_results['best-e'] = [len(history.history['loss'])-config.patience]
         else:
             fold_results['train-loss'].append(history.history['loss'])
             fold_results['train-accuracy'].append(history.history['accuracy'])
@@ -254,11 +275,19 @@ def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_se
             fold_results['precision'].append(p)
             fold_results['recall'].append(r)
             fold_results['fscore'].append(f)
+            fold_results['model'].append(model_name)
+            fold_results['best-e'].append(len(history.history['loss'])-config.patience)
 
         fold += 1
 
     elapsed = (time.time() - start)
     print("Training time (all folds):", str(timedelta(seconds=elapsed)))
     fold_results['training_time'] = elapsed
+
+    print(sklearn.metrics.classification_report(all_labels, all_predictions))
+    #conf_matrix = sklearn.metrics.confusion_matrix(all_labels, all_predictions)  # todo: add labels
+    #print(conf_matrix)
+    #ml_helpers.plot_confusion_matrix(conf_matrix)
+    #ml_helpers.plot_prediction_distribution(all_labels, all_predictions)
 
     return fold_results
