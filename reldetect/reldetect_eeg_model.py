aa737f46410949cf992421922f4503c9a87e400b
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-04-26 18:19:05 +0200

    ..

--- reldetect/reldetect_eeg_model.py
+++ reldetect/reldetect_eeg_model.py
@@ -109,7 +109,7 @@ def lstm_classifier(labels, eeg, embedding_type, param_dict, random_seed_value, 
         # callbacks for early stopping and saving the best model
         es = EarlyStopping(monitor='val_accuracy', mode='max', min_delta=config.min_delta, patience=config.patience)
         model_name = '../models/' + str(random_seed_value) + '_fold' + str(fold) + '_' + config.class_task + '_' + \
-                     config.feature_set[0] + '_' + d.strftime("%d/%m/%Y-%H:%M:%S") + '.h5'
+                     config.feature_set[0] + '_' + d.strftime("%d%m%Y-%H:%M:%S") + '.h5'
         mc = ModelCheckpoint(model_name, monitor='val_accuracy', mode='max', save_best_only=True, verbose=1)
 
         # train model
