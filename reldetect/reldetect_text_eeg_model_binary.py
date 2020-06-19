fb13bff7d57842569a22e556c9e333fa0562ab12
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-06-19 14:33:41 +0200

    ..

--- reldetect/reldetect_text_eeg_model_binary.py
+++ reldetect/reldetect_text_eeg_model_binary.py
@@ -45,6 +45,7 @@ def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_se
 
     # load eeg
     eeg_X, max_length_cogni = ml_helpers.prepare_cogni_seqs(eeg)
+    eeg_X = ml_helpers.scale_feature_values(eeg_X)
 
     print("Label distribution:")
     for cl in range(len(y[0])):
@@ -65,7 +66,7 @@ def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_se
 
 
     # prepare EEG data
-    eeg_X = ml_helpers.scale_feature_values(eeg_X)
+
     X_data_eeg = ml_helpers.pad_cognitive_feature_seqs(eeg_X, max_length_cogni, "eeg")
 
 
