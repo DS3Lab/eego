b8aa1c3098a2d8ddc2dcf138b52d75bc5018a5ee
Nora Hollenstein <nora.hollenstein@gmail.com>
2020-06-19 14:32:22 +0200

    ..

--- reldetect/reldetect_text_eeg_model_binary.py
+++ reldetect/reldetect_text_eeg_model_binary.py
@@ -43,29 +43,30 @@ def lstm_classifier(features, labels, eeg, embedding_type, param_dict, random_se
     X_text = list(features.keys())
     y = list(labels.values())
 
+    # load eeg
+    eeg_X, max_length_cogni = ml_helpers.prepare_cogni_seqs(eeg)
+
     print("Label distribution:")
     for cl in range(len(y[0])):
         class_count = [1 if int(n[cl]) == 1 else 0 for n in y]
         class_count = sum(class_count)
         print(cl, class_count)
 
+    y, X_text, eeg_X = ml_helpers.drop_samples(y, X_text, eeg_X)
+
+    print("Label distribution:")
+    for cl in range(len(y[0])):
+        class_count = [1 if int(n[cl]) == 1 else 0 for n in y]
+        class_count = sum(class_count)
+        print(cl, class_count)
 
     # prepare text samples
     X_data_text, num_words, text_feats = ml_helpers.prepare_text(X_text, embedding_type, random_seed_value)
 
-    # load eeg
-    eeg_X, max_length_cogni = ml_helpers.prepare_cogni_seqs(eeg)
+
     # prepare EEG data
     eeg_X = ml_helpers.scale_feature_values(eeg_X)
     X_data_eeg = ml_helpers.pad_cognitive_feature_seqs(eeg_X, max_length_cogni, "eeg")
-
-    y, X_data_text, X_data_eeg = ml_helpers.drop_samples(y, X_data_text, X_data_eeg)
-
-    print("Label distribution:")
-    for cl in range(len(y[0])):
-        class_count = [1 if int(n[cl]) == 1 else 0 for n in y]
-        class_count = sum(class_count)
-        print(cl, class_count)
 
 
 
