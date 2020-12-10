7360d068d68dd98134268b874b636804257259ca
bglaus <bglaus@student.ethz.ch>
2020-12-10 19:49:50 +0100

    added boolean to toggle feature extraction

--- config.py
+++ config.py
@@ -2,46 +2,45 @@
 #rootdir_zuco1 = "/Volumes/methlab/NLP/Ce_ETH/OSF-ZuCo1.0-200107/mat7.3/"
 #rootdir_zuco2 = "/Volumes/methlab/NLP/Ce_ETH/2019/FirstLevel_V2/"
 base_dir = "/mnt/ds3lab-scratch/noraho/coling2020/"
-#base_dir = '/Users/benjaminglaus/Desktop/BA_code/'
+#base_dir = '/Users/benjaminglaus/Desktop/BA_code/' 
 rootdir_zuco1 = base_dir+"zuco1/"
 rootdir_zuco2 = base_dir+"zuco2/"
 
 # subjects (subejcts starting with "Z" are from ZuCo 1, subjects starting with "Y" are from ZuCo 2)
 #subjects = ['YAC', 'YAG', 'YAK', 'YDG', 'YDR', 'YFR', 'YFS', 'YHS', 'YIS', 'YLS', 'YMD', 'YMS', 'YRH', 'YRK', 'YRP', 'YSD', 'YSL', 'YTL']  # exclude YMH
 subjects = ["ZKW", "ZJS", "ZDN", "ZJN", "ZPH", "ZAB", "ZJM", "ZKB", "ZKH", "ZMG", "ZGW", "ZKW", "ZDM"]
+
 #subjects = ["ZKW", "ZJS", "ZDN", "ZJN", "ZPH", "ZAB", "ZJM", "ZKB", "ZKH", "ZMG", "ZGW", "ZKW", "ZDM", 'YAC', 'YAG', 'YAK', 'YDG', 'YDR', 'YFR', 'YFS', 'YHS', 'YIS', 'YLS', 'YMD', 'YMS', 'YRH', 'YRK', 'YRP', 'YSD', 'YSL', 'YTL']
 
 # for running the experiments with previously extracted feature only one subject (from each dataset) is necessary
 
+run_eeg_extraction = False
 
 # ML task {sentiment-bin, sentiment-tri, ner, reldetect}
 class_task = 'sentiment-bin'
 # ML model {lstm, cnn}
 model = 'cnn'
+# word embeddings {none, glove (300d), bert}
+embeddings = 'bert'
 
 # features sets {'text_only' , 'eeg_raw', 'eeg_theta', 'eeg_alpha', 'eeg_beta', 'eeg_gamma', 'combi_eeg_raw', 'eye_tracking', 'combi_eye_tracking'}
 # sentence level features: {'combi_concat', 'sent_eeg_theta'}
 # combined models: {'eeg_eye_tracking', 'eeg4'}
-
-feature_set = ['eeg_theta']
-
-# word embeddings {none, glove (300d), bert}
-embeddings = 'bert'
+feature_set = ['eeg_alpha', 'random']
 
 # hyper-parameters to test - general
-lstm_dim = [128,256]
+lstm_dim = [64]
 lstm_layers = [1]
-dense_dim = [64,128]#,128]
-dropout = [0.3]
+dense_dim = [64]#,128]
+dropout = [0.5]
 batch_size = [40]
 epochs = [200]
-lr = [0.001, 0.0001, 0.00001]
+lr = [0.01]
 
-inception_filters = [14,16]
+inception_filters = [14]
 inception_kernel_sizes = [[1,4,7]]
-inception_pool_size = [5]
+inception_pool_size = [3]
 inception_dense_dim = [(128,16)]
-#inception_activation_function=['elu', 'relu']
 
 # best params raw eeg:
 eeg_lstm_dim = [64]
@@ -50,7 +49,7 @@ eeg_dropout = [0.1]
 
 # other parameters
 folds = 5
-random_seed_values = [13, 78]#, 22, 66, 42]
+random_seed_values = [13, 78, 22, 66, 42]  
 validation_split = 0.1
 patience = 80
 min_delta = 0.0000001
