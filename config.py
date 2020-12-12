c14cf9becb81f55195056b83c2f0a3cb315232f5
Benjamin Glaus <71631505+bglaus@users.noreply.github.com>
2020-12-12 20:18:21 +0100

    updated to run eeg4 model

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
-# TODO
-# maybe add a parameter to decide wheter or not to generate feature files
-# or load them from file, so we don't need to (un)comment code 
-
 # dataset directories
 #rootdir_zuco1 = "/Volumes/methlab/NLP/Ce_ETH/OSF-ZuCo1.0-200107/mat7.3/"
 #rootdir_zuco2 = "/Volumes/methlab/NLP/Ce_ETH/2019/FirstLevel_V2/"
-base_dir = "/Users/benjaminglaus/Desktop/BA code/"
+#base_dir = "/mnt/ds3lab-scratch/noraho/coling2020/"
+base_dir = '/Users/benjaminglaus/Desktop/BA_code/' 
 rootdir_zuco1 = base_dir+"zuco1/"
 rootdir_zuco2 = base_dir+"zuco2/"
 
@@ -13,36 +10,37 @@ rootdir_zuco2 = base_dir+"zuco2/"
 #subjects = ['YAC', 'YAG', 'YAK', 'YDG', 'YDR', 'YFR', 'YFS', 'YHS', 'YIS', 'YLS', 'YMD', 'YMS', 'YRH', 'YRK', 'YRP', 'YSD', 'YSL', 'YTL']  # exclude YMH
 subjects = ["ZKW", "ZJS", "ZDN", "ZJN", "ZPH", "ZAB", "ZJM", "ZKB", "ZKH", "ZMG", "ZGW", "ZKW", "ZDM"]
 
+#subjects = ["ZKW", "ZJS", "ZDN", "ZJN", "ZPH", "ZAB", "ZJM", "ZKB", "ZKH", "ZMG", "ZGW", "ZKW", "ZDM", 'YAC', 'YAG', 'YAK', 'YDG', 'YDR', 'YFR', 'YFS', 'YHS', 'YIS', 'YLS', 'YMD', 'YMS', 'YRH', 'YRK', 'YRP', 'YSD', 'YSL', 'YTL']
+
 # for running the experiments with previously extracted feature only one subject (from each dataset) is necessary
 
+run_eeg_extraction = False
 
 # ML task {sentiment-bin, sentiment-tri, ner, reldetect}
 class_task = 'sentiment-bin'
 # ML model {lstm, cnn}
-model = 'lstm'
+model = 'cnn'
+# word embeddings {none, glove (300d), bert}
+embeddings = 'glove'
 
 # features sets {'text_only' , 'eeg_raw', 'eeg_theta', 'eeg_alpha', 'eeg_beta', 'eeg_gamma', 'combi_eeg_raw', 'eye_tracking', 'combi_eye_tracking'}
 # sentence level features: {'combi_concat', 'sent_eeg_theta'}
 # combined models: {'eeg_eye_tracking', 'eeg4'}
-
-feature_set = ['eeg_alpha']
-
-# word embeddings {none, glove (300d), bert}
-embeddings = 'glove'
+feature_set = ['eeg4']
 
 # hyper-parameters to test - general
 lstm_dim = [64]
 lstm_layers = [1]
-dense_dim = [128]
-dropout = [0.5]
+dense_dim = [256]
+dropout = [0.3]
 batch_size = [40]
 epochs = [200]
-lr = [0.001]
+lr = [0.00001]
 
-cnn_filters = [16, 32, 64]
-cnn_kernel_size = [2, 3, 4]
-cnn_network = [['Conv', 'Conv', 'Pooling', 'Conv', 'Conv', 'Pooling']] #, ['Conv', 'Conv', 'Conv', 'Conv', 'Pooling']]
-
+inception_filters = [16]
+inception_kernel_sizes = [[1,4,7]]
+inception_pool_size = [5]
+inception_dense_dim = [(128,16)]
 
 # best params raw eeg:
 eeg_lstm_dim = [64]
@@ -51,7 +49,7 @@ eeg_dropout = [0.1]
 
 # other parameters
 folds = 5
-random_seed_values = [13, 78, 22, 66, 42]
+random_seed_values = [13, 78, 22, 66, 42]  
 validation_split = 0.1
 patience = 80
 min_delta = 0.0000001
@@ -61,3 +59,4 @@ drop_classes = [10, 8]#, 1, 4, 6, 3, 9, 2]
 # only for Relation Detection:
 rel_thresholds = [0.3, 0.5, 0.7]
 
+# EEG4+Bert
