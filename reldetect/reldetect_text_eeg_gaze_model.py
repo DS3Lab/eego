d750917f936adaf944382810e440a6815b2d4d92
Nora Hollenstein <nora.hollenstein@gmail.com>
2021-04-27 07:46:08 +0200

    ..

--- reldetect/reldetect_text_eeg_gaze_model.py
+++ reldetect/reldetect_text_eeg_gaze_model.py
@@ -212,10 +212,6 @@ def classifier(features, labels, eeg, gaze, embedding_type, param_dict, random_s
         batch_size = param_dict['batch_size']
         epochs = param_dict['epochs']
         lr = param_dict['lr']
-        inception_filters = param_dict['inception_filters']
-        inception_kernel_sizes = param_dict['inception_kernel_sizes']
-        inception_pool_size = param_dict['inception_pool_size']
-        inception_dense_dim = param_dict['inception_dense_dim']
 
         fold_results['params'] = [lstm_dim, lstm_layers, dense_dim, dropout, batch_size, epochs, lr, embedding_type,
                                   random_seed_value]
